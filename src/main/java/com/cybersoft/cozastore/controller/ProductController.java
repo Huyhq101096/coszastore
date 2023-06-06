efdc6c2cb8a6b01ff8dc281849171c24e1188c0f
huyhq101096@gmail.com <huyhq101096@gmail.com>
2023-06-06 22:10:33 +0700

    update

--- src/main/java/com/cybersoft/cozastore/controller/ProductController.java
+++ src/main/java/com/cybersoft/cozastore/controller/ProductController.java
@@ -1,14 +1,15 @@
 package com.cybersoft.cozastore.controller;
 
+import com.cybersoft.cozastore.payload.request.ProductRequest;
 import com.cybersoft.cozastore.payload.response.BaseResponse;
 import com.cybersoft.cozastore.service.impl.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
@@ -23,6 +24,11 @@ public class ProductController {
         BaseResponse baseResponse = new BaseResponse();
         baseResponse.setData(iProductService.getProductByCategoryId(id));
         return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+    }
 
+    @PostMapping("")
+    public ResponseEntity<?> addProduct(@Valid @RequestParam ProductRequest productRequest) {
+
+        return new ResponseEntity<>("",HttpStatus.OK);
     }
 }
